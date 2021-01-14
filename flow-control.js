759a1949994afbcd2e4c97ecdad267fd921ae173
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-14 22:26:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,7 +24,7 @@ return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
-return age >= 13 && age <= 19?  "you are a teenager!": "You are not a teenager";
+return age >= 13 && age <= 19?  "you are a teenager!": "You are not a teenager"
 }
 
 function switchAge(age) {
